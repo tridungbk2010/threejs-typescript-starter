0a8559b1ce7450168fef5adb8abff292c164da87
Chris <chris.ho@goodstorytime.com>
2024-04-02 23:07:31 +0700

    refactor code

--- src/webgl/Floor.ts
+++ src/webgl/Floor.ts
@@ -11,37 +11,18 @@ export default class Floor {
   constructor(_resources: Resources) {
     this.resources = _resources;
 
-    this.setGeometry();
     this.setTextures();
-    this.setMaterial();
-    this.setMesh();
 
-    // this.textures = {};
+    this.material = new THREE.MeshStandardMaterial({
+      map: this.textures.color,
+      normalMap: this.textures.normal,
+    });
 
-    // this.textures.color = resources.items.grassColorTexture;
-    // this.textures.color.colorSpace = THREE.SRGBColorSpace;
-    // this.textures.color.repeat.set(1.5, 1.5);
-    // this.textures.color.wrapS = THREE.RepeatWrapping;
-    // this.textures.color.wrapT = THREE.RepeatWrapping;
-
-    // this.textures.normal = resources.items.grassNormalTexture;
-    // this.textures.normal.repeat.set(1.5, 1.5);
-    // this.textures.normal.wrapS = THREE.RepeatWrapping;
-    // this.textures.normal.wrapT = THREE.RepeatWrapping;
-
-    // this.material = new THREE.MeshStandardMaterial({
-    //   map: this.textures.color,
-    //   normalMap: this.textures.normal,
-    // });
-
-    // this.geometry = new THREE.CircleGeometry(5, 64);
-
-    // this.mesh = new THREE.Mesh(this.geometry, this.material);
-    // this.mesh.rotation.x = -Math.PI * 0.5;
-    // this.mesh.receiveShadow = true;
-  }
-  setGeometry() {
     this.geometry = new THREE.CircleGeometry(5, 64);
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.rotation.x = -Math.PI * 0.5;
+    this.mesh.receiveShadow = true;
   }
 
   setTextures() {
@@ -57,19 +38,5 @@ export default class Floor {
     this.textures.normal.repeat.set(1.5, 1.5);
     this.textures.normal.wrapS = THREE.RepeatWrapping;
     this.textures.normal.wrapT = THREE.RepeatWrapping;
-  }
-
-  setMaterial() {
-    this.material = new THREE.MeshStandardMaterial({
-      map: this.textures.color,
-      normalMap: this.textures.normal,
-    });
-  }
-
-  setMesh() {
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.mesh.rotation.x = -Math.PI * 0.5;
-    this.mesh.receiveShadow = true;
-    // this.scene.add(this.mesh);
   }
 }
